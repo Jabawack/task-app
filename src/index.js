859a2b36a1cdaaa2520916d6bedf71736411e12c
TK <tk@rcinnov.com>
2020-12-08 20:46:22 +0900

    reorder a list

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './css-reset.css';
+import {DragDropContext} from 'react-beautiful-dnd';
 import initialData from './initial-data';
 import Column from './column';
 
@@ -9,13 +10,21 @@ import Column from './column';
 class App extends React.Component {
     state = initialData;
 
+    onDragEnd = result => {
+        ;
+    };
+
     render() {
-        return this.state.columnOrder.map((columnId) => {
-            const column = this.state.columns[columnId];
-            const tasks = column.taskIds.map(taskId => this.state.tasks[taskId]);
-            
-            return <Column key={column.id} column={column} tasks={tasks}/>;
-        });
+        return (
+            <DragDropContext onDragEnd={this.onDragEnd}>
+                {this.state.columnOrder.map((columnId) => {
+                    const column = this.state.columns[columnId];
+                    const tasks = column.taskIds.map(taskId => this.state.tasks[taskId]);
+
+                    return <Column key={column.id} column={column} tasks={tasks}/>;
+                })}
+            </DragDropContext>
+        )
     }
 }
 
