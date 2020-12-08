dfdba52ecbf0a0e06e9dfc6019f7fecba8f6226b
TK <tk@rcinnov.com>
2020-12-09 00:44:20 +0900

    persist list reordering

---
+++ src/example-result.js
@@ -0,0 +1,13 @@
+const result = {
+    draggableId: 'task-1',
+    type: 'TYPE',
+    reason: 'DROP',
+    source: {
+        draggableId: 'colum-1',
+        index: 0,
+    },
+    destination: {
+        droppableId: 'colum-1',
+        index: 1,
+    }
+}
--- src/index.js
+++ src/index.js
@@ -11,7 +11,44 @@ class App extends React.Component {
     state = initialData;
 
     onDragEnd = result => {
-        ;
+        const {destination, source, draggableId} = result;
+        console.log(result);
+        
+        if (!destination) {
+            return;
+        }
+
+        if (destination.droppableId === source.droppableId && destination.index === source.index) {
+            return;
+        }
+
+        const column = this.state.columns[source.droppableId];
+        const newTaskIds = Array.from(column.taskIds);
+        newTaskIds.splice(source.index, 1);
+        newTaskIds.splice(destination.index, 0, draggableId);
+        
+        const newColumn = {
+            ...column,
+            taskIds: newTaskIds,
+        };
+        
+        const newState = {
+            ...this.state,
+            columns: {
+                ...this.state.columns,
+                [newColumn.id]: newColumn,
+            },
+        };
+        
+        this.setState(newState);
+
+//        const column = reorder(
+//            tierState.quotes,
+//            result.source.index,
+//            result.destination.index
+//        );
+
+//        setTierState({tierData});
     };
 
     render() {
