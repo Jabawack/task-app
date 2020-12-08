789f19184a7873c03573b3f2ebf808a2f9d6c4f7
TK <tk@rcinnov.com>
2020-12-09 04:19:01 +0900

    designate control of dragging - handle

--- src/task.jsx
+++ src/task.jsx
@@ -8,6 +8,16 @@ border-radius: 2px;
 padding: 8px;
 margin-bottom: 8px;
 background-color: ${props => (props.isDragging ? 'lightgreen' : 'white')};
+
+display: flex;
+`;
+
+const Handle = styled.div`
+width: 20px;
+height: 20px;
+background-color: orange;
+border-radius 4px;
+margin-right: 8px;
 `;
 
 export default class Task extends React.Component {
@@ -18,11 +28,11 @@ export default class Task extends React.Component {
                 {(provided, snapshot) => (
                     <Container
                         {...provided.draggableProps}
-                        {...provided.dragHandleProps}
                         ref={provided.innerRef}
                         isDragging={snapshot.isDragging}
                         >
-                            {this.props.task.content}
+                        <Handle {...provided.dragHandleProps}/>
+                        {this.props.task.content}
                     </Container>
                 )}
             </Draggable>
